6696a2a245282a3f5441bb34fa860afc3d7f8958
Marck C. Guzmán <marckcarrion@gmail.com>
2019-03-27 22:51:47 +0100

    Created the class Reader for exift tag, all needed method created for it

--- __init__.py
+++ __init__.py
@@ -2,6 +2,23 @@ from exifreader import reader
 # url of image that will read the exif tags
 # TODO: en el archivo reader crear una funcion que selecione solo ciertas etiquetas exif
 img_url = 'img.jpeg'
-tags = reader.read(img_url)
-for key in tags.keys():
-    print("{} : {}".format(key, tags.get(key)))
+
+
+def show(data):
+    """
+    Print map
+    :param data: map to print
+    :return:
+    """
+    for key in data.keys():
+        print("{}: {}".format(key, data.get(key)))
+
+
+r = reader.Reader(img_url)
+# show all data
+print("="*40, "\n", "ALL")
+show(r.get_data())
+# set the filter to show
+print("="*40, "\n", "FILTER")
+filter = ('Image Make', 'EXIF ExposureTime', 'EXIF FNumber', 'EXIF ISOSpeedRatings', 'Image Software')
+show( r.get_filter_tag(filter))
--- exifreader/reader.py
+++ exifreader/reader.py
@@ -1,12 +1,73 @@
+# MIT License
+#
+# Copyright (c) 2019 MARCK C. GUZMAN
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 import exifread
-# reed all exif tag from image
-def read(img):
-    # open the img file in binary mode and only to read
-    with open(img, "rb") as file:
-        # set the file content to the reader
-        data = exifread.process_file(file)
-        # in data we have a hash map with all exif tags
-        file.close()
-        return data
-# para seleccinar las etiquetas primero crearemos una lista y luego comprobaremos si
-# la clave está en esa lista. Para la comprobación podremos utilizar 'in': if var in ('a','b')
+
+
+class Reader:
+    """
+    Class for work with exif tags
+    """
+    data = None
+
+    def __init__(self, img):
+        self.image = img
+    # reed all exif tag from image
+
+    def __read__(self):
+        """
+        Read the image exif tags
+        """
+        # open the img file in binary mode and only to read
+        with open(self.image, "rb") as file:
+            # set the file content to the reader
+            self.data = exifread.process_file(file)
+            # in data we have a hash map with all exif tags
+            file.close()
+
+    def get_data(self):
+        """
+
+        :return: full image exif tags
+        """
+        if self.data is None:
+            self.__read__()
+        return self.data
+
+    def set_image(self, img):
+        self.image = img
+
+    # para seleccinar las etiquetas primero crearemos una lista y luego comprobaremos si
+    # la clave está en esa lista. Para la comprobación podremos utilizar 'in': if var in ('a','b')
+    def get_filter_tag(self, filter_list):
+        """:param filter_list list of tags to return
+        :return map with the requires tags and values
+        """
+        if filter_list is None:
+            return -1
+        map_out = {}
+        for key in self.get_data():
+            if key in filter_list:
+                map_out[key] = self.get_data().get(key)
+        return map_out
+
+
+

da9810e2d0b61bba7e506be9d0646734ab427c5d
Marck C. Guzmán <marckcarrion@gmail.com>
2019-04-21 22:38:55 +0200

    Removed a semicolon from __api__.py

--- image_manager/__api__.py
+++ image_manager/__api__.py
@@ -1,10 +1,15 @@
 from Crypto.Cipher import AES
+
+
 def aes_key():
     return '6pú2BÑq9pfI.Ú'
+
+
 def api_key():
     suit = AES.new(key=aes_key())
     with open('api_key.k', "rb") as f:
         key = f.read()
         return suit.decrypt(key).decode('utf-8')
 
-API_KEY = api_key();
+
+API_KEY = api_key()
